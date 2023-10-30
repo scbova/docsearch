898e7212953e2be0145086d2a89a50a9a093b655
scbova <sbova@ucsd.edu>
2023-10-30 15:38:31 -0700

    Update TestDocSearch.java

--- TestDocSearch.java
+++ TestDocSearch.java
@@ -9,7 +9,7 @@ public class TestDocSearch {
 	public void testIndex() throws URISyntaxException, IOException {
     Handler h = new Handler("./technical/");
     URI rootPath = new URI("http://localhost/");
-    assertEquals("There are 1391 total files to search.", h.handleRequest(rootPath));
+    assertEquals("There are 1392 total files to search.", h.handleRequest(rootPath));
 	}
 	@Test 
 	public void testSearch() throws URISyntaxException, IOException {
@@ -32,6 +32,14 @@ public class TestDocSearch {
     Handler h = new Handler("./technical/");
     URI rootPath = new URI("http://localhost/search?q=pizza");
     String expect = "Found 2 paths:\n./technical/911report/chapter-7.txt\n./technical/plos/journal.pbio.0020012.txt";
+    assertEquals(expect, h.handleRequest(rootPath));
+	}
+
+    @Test 
+	public void testSearch3() throws URISyntaxException, IOException {
+    Handler h = new Handler("./technical/");
+    URI rootPath = new URI("http://localhost/search?title=my-text");
+    String expect = "Found 1 paths:\n./technical/911report/my-text.txt\n";
     assertEquals(expect, h.handleRequest(rootPath));
 	}
 }
