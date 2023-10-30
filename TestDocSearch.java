a9c1b006b8c3173354505f4c116e123f622d2cda
scbova <sbova@ucsd.edu>
2023-10-30 15:20:19 -0700

    Update TestDocSearch.java

--- TestDocSearch.java
+++ TestDocSearch.java
@@ -18,5 +18,21 @@ public class TestDocSearch {
     String expect = "Found 2 paths:\n./technical/biomed/ar615.txt\n./technical/plos/journal.pbio.0020150.txt";
     assertEquals(expect, h.handleRequest(rootPath));
 	}
+
+    @Test
+    public void testSearch2() throws URISyntaxException, IOException {
+        Handler h = new Handler("./technical/");
+        URI rootPath = new URI("http://localhost/search?q=hello");
+        String expect = "Found 0 paths:\n";
+        assertEquals(expect, h.handleRequest(rootPath));
+    }
+
+    @Test 
+	public void testSearch3() throws URISyntaxException, IOException {
+    Handler h = new Handler("./technical/");
+    URI rootPath = new URI("http://localhost/search?q=pizza");
+    String expect = "Found 2 paths:\n./technical/911report/chapter-7.txt\n./technical/plos/journal.pbio.0020012.txt";
+    assertEquals(expect, h.handleRequest(rootPath));
+	}
 }
 
